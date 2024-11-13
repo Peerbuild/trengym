9c70cc865b8a53712b1c33e11944b46a2711f361
Priyobroto Kar <priyobrotokar@gmail.com>
2024-11-13 14:25:59 +0530

    feat(admin,api): initialize admin app with configuration files and components; update API server port

--- apps/admin/app/index.tsx
+++ apps/admin/app/index.tsx
@@ -7,7 +7,7 @@ const HomeScreen = () => {
   return (
     <Theme>
       <View className="flex-1 justify-start gap-2 px-10 pt-60">
-        <Text className="text-xl">Member</Text>
+        <Text className="text-xl">Admin</Text>
         <Text className="text-3xl">TrenGym</Text>
         <Text className="text-lg">
           An app brought you by <Text className="font-bold">Peerbuild</Text>
