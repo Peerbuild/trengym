f4b7e030321ae9cfc8ad2d058aa3ab6674e0ae08
Priyobroto Kar <priyobrotokar@gmail.com>
2024-12-03 02:17:07 +0530

    feat(chore): add Twilio credentials to test environment variables

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { IsPublic } from './decorators/public.decorator'
 
+@IsPublic()
 @Controller('hello')
 export class AppController {
   constructor(private readonly appService: AppService) {}
